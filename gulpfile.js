0657187981b4c5a7e9655b8f56df101c1204d80b
Alexander Komarov <komar81@gmail.com>
2019-02-09 16:44:03 +0300

    service poligraphy section is done on desktp

--- gulpfile.js
+++ gulpfile.js
@@ -49,7 +49,7 @@ gulp.task('js', gulp.series('concat-scripts', 'compressjs'));
 gulp.task('images', function () {
   return gulp.src('source/img/**/*.{png,jpg,svg}')
     .pipe(imagemin([
-      imagemin.optipng({optimizationLevel: 4}),
+      imagemin.optipng({optimizationLevel: 3}),
       imagemin.jpegtran({progressive: true}),
       imagemin.svgo()
     ]))
